d490b7490fcfa86666a60052adc42e937f53bc1b
Humayra Akter <96544744+Humayra-Akter@users.noreply.github.com>
2024-05-25 23:44:37 +0600

    layout added

---
+++ src/routes/routes.jsx
@@ -0,0 +1,42 @@
+import { createBrowserRouter } from "react-router-dom";
+import Home from "../pages/Home";
+import About from "../pages/About";
+import Login from "../pages/Login";
+import MainLayout from "../layouts/MainLayout";
+import ErrorPage from "../pages/ErrorPage";
+import DashboardLayout from "../layouts/DashboardLayout";
+import Dashboard from "../pages/Dashboard";
+
+export const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <MainLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+
+      {
+        path: "/about",
+        element: <About />,
+      },
+      {
+        path: "/login",
+        element: <Login />,
+      },
+    ],
+  },
+  {
+    path: "dashboard",
+    element: <DashboardLayout />,
+    errorElement: <ErrorPage />,
+    children: [
+      {
+        path: "home",
+        element: <Dashboard />,
+      },
+    ],
+  },
+]);
